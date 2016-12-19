7781dded8d0547ecd4b424d8a8c4ea07b0909fe8
ManuGarcia <ReservoirDog@ManuGarcias-MacBook-Pro-2.local>
2016-12-18 22:14:04 -0600

    uploading project to github

---
+++ app.js
@@ -0,0 +1,54 @@
+(function() {
+
+    var app = angular.module('funwithcountries', ['ngRoute']);
+
+    app.factory('countryService', function($http) {
+        var baseUrl = 'services/';
+
+        return {
+          getCountries: function() {
+              return $http.get(baseUrl + 'getCountries.php');
+          },
+          getStates: function(countryCode) {
+              return $http.get(baseUrl + 'getStates.php?countryCode=' +
+                encodeURIComponent(countryCode));
+          }
+        };
+    })
+
+    app.controller('CountryController', function(countryService) {
+
+        var that = this;
+
+        countryService.getCountries().success(function(data) {
+            that.countries = data;
+        });
+
+    });
+
+    app.config(function($routeProvider) {
+       $routeProvider.when('/states/:countryCode', {
+           templateUrl: 'state-view.html',
+           controller: function($routeParams, countryService) {
+               this.params = $routeParams;
+
+               var that = this;
+
+               countryService.getStates(this.params.countryCode || "").success(function(data) {
+                   that.states = data;
+               })
+
+               this.addStateTo = function() {
+                   if (!this.states) {
+                       this.states = [];
+                   }
+                   this.states.push({name: this.newState});
+                   this.newState = "";
+               };
+
+           },
+           controllerAs: 'stateCtrl'
+       });
+    });
+
+})();
---
+++ classes/CountryRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'Country.php';
+require 'State.php';
+
+class CountryRepository {
+
+    private static $countries = array();
+
+    protected static function init() {
+        $countries = array();
+
+        array_push($countries,
+          new Country('Austria', 'at', array(
+              new State('Styria'), new State('Tyrol')
+          )));
+        array_push($countries,
+            new Country('Canada', 'ca', array(
+                new State('Ontario'), new State('Quebec')
+            )));
+        array_push($countries,
+            new Country('Luxembourg', 'lu'));
+
+        self::$countries = $countries;
+    }
+
+    public static function getCountries() {
+        if (count(self::$countries) === 0) {
+            self::init();
+        }
+        return self::$countries;
+    }
+
+    public static function getStates($countryCode) {
+        if (count(self::$countries) === 0) {
+            self::init();
+        }
+        $country = array_filter(self::$countries, function($c) use ($countryCode) {
+            return $c->code === $countryCode;
+        });
+        if (count($country) === 0) {
+            return array();
+        }
+        $firstCountry = array_shift($country);
+        return $firstCountry->states;
+    }
+
+} 
